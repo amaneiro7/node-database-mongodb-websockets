3eb9f42d5c9327ee38b67db64ae6c44e88e660ab
Andres Maneiro <amaneiro7@gmail.com>
2023-06-27 13:02:25 -0400

    controller

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,18 +1,27 @@
-export default class ControllerMessage {
-    constructor() {}
-    addMessage(user, message) {
+import Store from './store.js'
+export default class Controller {    
+    constructor() {
+        this.store = new Store()
+    }
+    add(user, message) {
         return new Promise((resolve, reject) => {
             if (!user || !message) {
                 console.error('[messageControler] No hay usuario o mensaje');
                 throw new Error('Los datos son incorrectos')
-                return false
-            } 
-            resolve({
+            }
+            const fullMessage = {
                 user,
                 message,
                 date: this.addTime()
-            })            
+            }
+            this.store.add(fullMessage)            
+            resolve(fullMessage)            
         })
+    }
+    getAll() {
+        return new Promise((resolve, reject) => {
+            resolve(this.store.getAll())
+        })        
     }
     
     addTime() {
