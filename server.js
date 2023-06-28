851df3232459499c472aeefe19c2048dcf0f2df4
Andres Maneiro <amaneiro7@gmail.com>
2023-06-28 12:21:02 -0400

    badge, examen y certificado

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 socket.connect(server)
 
 db(config.mongoDB.uri)
-app.unsubscribe(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(router)
